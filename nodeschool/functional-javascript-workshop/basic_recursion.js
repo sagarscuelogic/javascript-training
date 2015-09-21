b538ef04339919e16a8fce3910b0b258726f9b73
sagarscuelogic <sagar.sutaria@cuelogic.co.in>
2015-09-21 19:36:45 +0530

    Studied with Stream specific code and started with functional javascript

--- nodeschool/functional-javascript-workshop/basic_recursion.js
+++ nodeschool/functional-javascript-workshop/basic_recursion.js
@@ -1,5 +1,10 @@
-function reduce (arr, fn, initial) {
-	
+function reduce(arr, fn, initial) {
+
 }
 
 module.exports = reduce;
+
+return inputWords.reduce(function(wordCountObject, word) {
+	wordCountObject[word] = ++wordCountObject[word] || 1;
+	return wordCountObject;
+}, {});
